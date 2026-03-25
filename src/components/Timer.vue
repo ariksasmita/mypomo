84edc05b92f385ba9e8f9024ec7289e96eabbf52
Arik Sasmita <arik.sasmita@dana.id>
2026-03-25 18:23:47 +0800

    feat: inputs

--- src/components/Timer.vue
+++ src/components/Timer.vue
@@ -2,14 +2,20 @@
   <div>
     <div>{{timeString}}</div>
     <button @click="buttonHandler">{{buttonText}}</button>
+    <div>
+      <input type="number" v-model="minInput" placeholder="min">
+      <input type="number" v-model="secInput" placeholder="sec">
+    </div>
   </div>
 </template>
 
 <script setup lang="ts">
   import { ref, onUnmounted, computed } from 'vue'
 
-  const minutes = ref(0)
-  const seconds = ref(5)
+  const minutes = ref(2)
+  const seconds = ref(0)
+  const minInput = ref(0)
+  const secInput = ref(0)
   const buttonText = ref('Start')
   let interv: any
 
@@ -25,10 +31,12 @@
       seconds.value = seconds.value - 1
     } else if (seconds.value === 0 && minutes.value > 0) {
       seconds.value = 59
+    } else {
+      resetTimer()
     }
 
     // minutes
-    if (minutes.value > 0) {
+    if (minutes.value > 0 && seconds.value === 0) {
       minutes.value = minutes.value - 1
     }
   }
@@ -39,6 +47,8 @@
 
   const startTimer = () => {
     if (interv) return
+    if (minInput.value) minutes.value = minInput.value
+    if (secInput.value) seconds.value = secInput.value
     buttonText.value = 'Stop'
     interv = setInterval(() => {
       updateTime()
@@ -48,6 +58,10 @@
   const stopTimer = () => {
     if (!interv) return
     clearInterval(interv)
+    resetTimer()
+  }
+
+  const resetTimer = () => {
     minutes.value = 0
     seconds.value = 0
     buttonText.value = 'Start'
